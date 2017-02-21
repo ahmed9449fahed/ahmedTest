7979b21ac52b209385ee93f358d546cfcdf83417
Ahmed Fahed <Fahed@soft-park.de>
2017-02-21 14:51:02 +0100

    unit test

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+namespace WindowsFormsApplication1.test
+{
+    [TestFixture]
+    public class Program
+    {
+    }
+}
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -20,10 +20,7 @@ namespace WindowsFormsApplication1
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hallo");
-
             MessageBox.Show("Hallo");
-
-
         }
     }
 }
